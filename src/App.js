3fe9dcc5181cfeb67e872cb8f8e846784839d17d
suhyun751207 <23s607h0659@gmail.com>
2025-02-03 15:10:04 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from "./Home.js";
-import Input from "./content/input.js";
 import Error from "./error.js";
 import "./App.css";
 
@@ -11,7 +10,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/0615/suhyun/system/data" element={<Input />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
